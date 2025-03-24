fd5575613afbd53dd5338212457129587d571b81
Nguyen Xuan Quang <quang456a@gmail.com>
2025-03-24 11:24:06 +0700

    add another cpn, layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import "./App.css";
+import "materialize-css/dist/css/materialize.min.css";
+
+import { useEffect } from "react";
+import "materialize-css/dist/js/materialize.min.js";
+import M from "materialize-css";
+import SearchBar from "./components/layout/SearchBar";
+import Logs from "./components/logs/Logs";
+
 
 function App() {
-  return <div>
-    My App
-  </div>;
+  useEffect(() => {
+    M.AutoInit();
+  });
+  return (
+    <>
+      <SearchBar />
+      <div className="container">
+        <Logs />
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/layout/PreLoader.tsx
@@ -0,0 +1,9 @@
+const PreLoader = () => {
+    return (
+      <div className="progress blue lighten-4">
+        <div className="indeterminate blue"></div>
+      </div>
+    );
+  };
+  
+  export default PreLoader;
